81968729e37d24c33727ca79d5faad646d415550
Fredrik Baberg <fbaberg@kth.se>
2018-06-10 01:33:26 +0200

    Initial preparations for new version.

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # hassio-addons [![Build Status](https://travis-ci.org/fredrikbaberg/hassio-addons.svg?branch=build)](https://travis-ci.org/fredrikbaberg/hassio-addons)
+
 Intended home for hassio addons, if time allows.
 
 ## octoprint
+
 The docker image for octoprint is also available on docker hub (fredrikbaberg).
 
 Please report issues here, or through the following [thread](https://community.home-assistant.io/t/repository-octoprint-wip/22883).
+
+## octoprint_dev
+
+Development version of octoprint addon. May not be stable for use.
--- octoprint_dev/build_test.sh
+++ octoprint_dev/build_test.sh
@@ -3,4 +3,4 @@
 mkdir -p my_test_data && mkdir -p my_test_config
 docker build --build-arg BUILD_FROM="homeassistant/amd64-base:latest" -t local/octoprint_test .
 # docker run -it --rm -v $(pwd)/my_test_data:/data -v $(pwd)/my_test_config:/config -p 5000 --device=/dev/ttyUSB0:/dev/ttyUSB0:rwm local/octoprint_test
-docker run -it --rm -v $(pwd)/my_test_data:/data -v $(pwd)/my_test_config:/config -p 5000 --device=/dev/ttyUSB0:/dev/ttyUSB0:rwm --entrypoint /bin/sh local/octoprint_test
+docker run -it --rm -v $(pwd)/my_test_data:/data -v $(pwd)/my_test_config:/config -p 5000:5000 --entrypoint /bin/sh local/octoprint_test
