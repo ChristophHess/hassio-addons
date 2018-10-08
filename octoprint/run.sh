faa48f10af9a7210f4deee6eb97ee855d7bdb839
Fredrik Baberg <fbaberg@kth.se>
2018-10-09 00:44:06 +0200

    Update in progress.

--- octoprint/run.sh
+++ octoprint/run.sh
@@ -23,5 +23,28 @@ setup() {
     fi
 }
 
-setup
-/data/OctoPrint/venv/bin/octoprint daemon restart --basedir /config/octoprint >> /home/octoprint/log_octo.txt
+create_config() {
+    if [ ! -f /config/octoprint/config.yaml ]; then
+        mkdir -p /config/octoprint/
+        cd /config/octoprint
+        touch config.yaml
+        echo "server:" >> config.yaml
+        echo "  commands:" >> config.yaml
+        echo "    serverRestartCommand: supervisorctl restart octoprint" >> config.yaml
+        echo "devel:" >> config.yaml
+        echo "  virtualPrinter:" >> config.yaml
+        echo "    enabled: true" >> config.yaml
+        echo "plugins:" >> config.yaml
+        echo "  cura:" >> config.yaml
+        echo "    cura_engine: /sbin/CuraEngine" >> config.yaml
+        echo "    debug_logging: false" >> config.yaml
+        echo "webcam:" >> config.yaml
+        echo "  ffmpeg: /usr/bin/ffmpeg" >> config.yaml
+    fi
+}
+
+create_config
+
+# setup
+/usr/bin/supervisord -c /etc/supervisord.conf
+# /data/OctoPrint/venv/bin/octoprint daemon restart --basedir /config/octoprint >> /home/octoprint/log_octo.txt
