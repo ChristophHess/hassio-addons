358c07219f46af0a118e229609c9ad23b049ad19
Fredrik Baberg <fbaberg@kth.se>
2018-10-09 15:47:36 +0200

    Almost done

--- octoprint/run.sh
+++ octoprint/run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -e
 
+copy_data() {
+    if [ ! -d /data/python ]; then
+        cp -R /root/python /data/
+    fi
+}
+
 create_config() {
     if [ ! -f /config/octoprint/config.yaml ]; then
         mkdir -p /config/octoprint/
@@ -12,17 +18,21 @@ create_config() {
         echo "devel:" >> config.yaml
         echo "  virtualPrinter:" >> config.yaml
         echo "    enabled: true" >> config.yaml
+        echo "folder:" >> config.yaml
+        echo "  timelapse: /config/timelapse" >> config.yaml
+        echo "  uploads: /config/uploads" >> config.yaml
+        echo "  watched: /config/watched" >> config.yaml
         echo "plugins:" >> config.yaml
         echo "  cura:" >> config.yaml
         echo "    cura_engine: /sbin/CuraEngine" >> config.yaml
         echo "    debug_logging: false" >> config.yaml
+        echo "  pluginmanager:" >> config.yaml
+        echo "    pip_force_user: true" >> config.yaml
         echo "webcam:" >> config.yaml
         echo "  ffmpeg: /usr/bin/ffmpeg" >> config.yaml
     fi
 }
 
+copy_data
 create_config
-
-# setup
-# export PYTHONPATH=${PYTHONPATH}:/data/plugins
 /usr/bin/supervisord -c /etc/supervisord.conf
