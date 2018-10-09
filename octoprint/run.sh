968e33e4c05b0eb664736b2d0c847df875b0849e
Fredrik Baberg <fbaberg@kth.se>
2018-10-09 02:08:28 +0200

    Need to add virtualenv to have persistent plugins.

--- octoprint/run.sh
+++ octoprint/run.sh
@@ -1,28 +1,6 @@
 #!/bin/sh
 set -e
 
-setup() {
-    sudo mkdir -p /config/octoprint
-    sudo chown -R octoprint:octoprint /config/octoprint
-    if [ ! -f /config/octoprint/config.yaml ]; then
-        sudo cp -a $HOME/.octoprint/config.yaml /config/octoprint/config.yaml
-        echo "Copy .octoprint/config.yaml"
-    fi
-    sudo chown -R octoprint:octoprint /data/
-    if [ ! -d /data/CuraEngine ]; then
-        sudo cp -a $HOME/CuraEngine /data/
-        echo "Copied CuraEngine"
-    fi
-    if [ ! -d /data/OctoPrint ]; then
-        sudo cp -a $HOME/OctoPrint /data/
-        echo "Copied OctoPrint"
-    fi
-    if [ ! -d /data/libArcus ]; then
-        sudo cp -a $HOME/libArcus /data/
-        echo "Copied libArcus"
-    fi
-}
-
 create_config() {
     if [ ! -f /config/octoprint/config.yaml ]; then
         mkdir -p /config/octoprint/
@@ -30,7 +8,7 @@ create_config() {
         touch config.yaml
         echo "server:" >> config.yaml
         echo "  commands:" >> config.yaml
-        echo "    serverRestartCommand: supervisorctl restart octoprint" >> config.yaml
+        echo "    serverRestartCommand: supervisorctl reload" >> config.yaml
         echo "devel:" >> config.yaml
         echo "  virtualPrinter:" >> config.yaml
         echo "    enabled: true" >> config.yaml
@@ -46,5 +24,5 @@ create_config() {
 create_config
 
 # setup
+# export PYTHONPATH=${PYTHONPATH}:/data/plugins
 /usr/bin/supervisord -c /etc/supervisord.conf
-# /data/OctoPrint/venv/bin/octoprint daemon restart --basedir /config/octoprint >> /home/octoprint/log_octo.txt
